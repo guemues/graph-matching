551c31663e21bd2abc0768784af41258bb5b0297
Orcun Gümüs <guemues@vpn-254-251.epfl.ch>
2018-04-11 23:56:00 +0200

    little fix

--- matching/creation.py
+++ matching/creation.py
@@ -159,7 +159,7 @@ def get_embeddings(graph, embedding_algorithm_enum, dimension_count, lower=None,
         A = np.array([np.histogram([graph.degree(i)] + [graph.degree(neig) for neig in graph.neighbors(i)], bins=dimension_count, density=True, range=(lower, higher))[0] for i in graph.nodes()])
         return A
     elif embedding_algorithm_enum is EmbeddingType.DegreeNeigDistribution:
-        A = np.array([np.histogram([graph.degree(i)] + [graph.degree(neig) for neig in graph.neighbors(i)] + [graph.degree(neigneig) for neigneig in graph.neighbors(neig) for neig in graph.neighbors(i)], bins=dimension_count, density=True, range=(lower, higher))[0] for i in graph.nodes()])
+        A = np.array([np.histogram([graph.degree(i)] + [graph.degree(neig) for neig in graph.neighbors(i)] + [graph.degree(neigneig) for neig in graph.neighbors(i) for neigneig in graph.neighbors(neig)], bins=dimension_count, density=True, range=(lower, higher))[0] for i in graph.nodes()])
         return A
     else:
         raise NotImplementedError
