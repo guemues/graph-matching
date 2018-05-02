1b36695ba4d78168deda24fc94b6461b62168b2f
Orcun Gumus <guemues@tsf-476-wpa-4-120.epfl.ch>
2018-05-02 19:17:01 +0200

    util added

--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import json
 import pandas as pd
@@ -5,28 +6,42 @@ from pandas import DataFrame
 
 from matching.statistics import find_tp_fp_fn
 
-info_file_str = './results/info.json'
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument('--hyperparameter', dest='hyperparameter', type=bool, help='')
+parser.add_argument('--output', dest='output', type=str, help='')
+parser.add_argument('--input', dest='input', type=str, help='')
+
+args = parser.parse_args()
+
+info_file_str = os.path.join(args.input, 'info.json')
 with open(info_file_str) as info_file:
     infos = json.load(info_file)
 
-hyperparamater_dict = {}
 
-for simulation_id, simulation_dict in infos.items():
-    if simulation_dict['hyperparameter'] in hyperparamater_dict:
-        hyperparamater_dict[simulation_dict['hyperparameter']].append(simulation_id)
-    else:
-        hyperparamater_dict[simulation_dict['hyperparameter']] = [simulation_id]
+def create_hyperparameter_result(input_folder, output_file):
+    hyperparamater_dict = {}
 
-total: DataFrame = pd.DataFrame()
-for hyperparameter, simulation_ids in hyperparamater_dict.items():
-    for simulation_id in simulation_ids:
-        _ = pd.read_csv(os.path.join('./results/', str(simulation_id) + '.csv'))
-        _['main_graph'] = str(simulation_id) + '_' + _['main_graph'].astype(str)
-        _['hyperparameter'] = str(hyperparameter)
-        total = total.append(find_tp_fp_fn(_))
+    for simulation_id, simulation_dict in infos.items():
+        if simulation_dict['hyperparameter'] in hyperparamater_dict:
+            hyperparamater_dict[simulation_dict['hyperparameter']].append(simulation_id)
+        else:
+            hyperparamater_dict[simulation_dict['hyperparameter']] = [simulation_id]
 
-total = total.drop(['Unnamed: 0'], axis=1)
+    total: DataFrame = pd.DataFrame()
+    for hyperparameter, simulation_ids in hyperparamater_dict.items():
+        for simulation_id in simulation_ids:
+            _ = pd.read_csv(os.path.join(input_folder, str(simulation_id) + '.csv'))
+            _['main_graph'] = str(simulation_id) + '_' + _['main_graph'].astype(str)
+            _['hyperparameter'] = str(hyperparameter)
+            total = total.append(find_tp_fp_fn(_))
 
-total.to_csv('results.csv', index=False)
+    total = total.drop(['Unnamed: 0'], axis=1)
+
+    total.to_csv(output_file, index=False)
 
 
+if __name__ == '__main__':
+
+    if args.hyperparameter:
+        create_hyperparameter_result(args.input, args.output)
